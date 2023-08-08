dc0c900fb705fe31f16c370fd3c612e2657a4e07
Afzalkhan377 <97460290+Afzalkhan377@users.noreply.github.com>
2023-08-07 20:43:43 -0400

    m

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import user from "../models/Users.js";
 import User from "../models/User.js";
 /*register user*/
 export const register =async(req,res)=>{

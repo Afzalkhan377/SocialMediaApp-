fff9d0e2e7ad49ca75bbc831e1e8047979f932ee
Afzalkhan377 <97460290+Afzalkhan377@users.noreply.github.com>
2023-08-07 20:31:01 -0400

    fix

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ import postRoutes from "./routes/posts.js"
 import {register} from "./controllers/auth.js";
 import {createPost} from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+
 /*CONFIGURATION*/
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -55,6 +59,10 @@ mongoose.connect(process.env.MONGO_URL , {
 })
 .then (()=>{
     app.listen(PORT,()=>console.log(`Server Port: ${PORT}`));
+
+  /* ADD DATA ONE TIME */
+   //User.insertMany(users);
+   // Post.insertMany(posts);
 
 })
 .catch((error)=> console.log(`${error} did not connect`));

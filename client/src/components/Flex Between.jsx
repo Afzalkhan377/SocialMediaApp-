8b879db292317a1ee8dfac54b9b702c2fc906222
Afzalkhan377 <97460290+Afzalkhan377@users.noreply.github.com>
2023-08-14 22:11:38 -0400

    updating frontend code

---
+++ client/src/components/Flex Between.jsx
@@ -0,0 +1,9 @@
+import {Box} from "@mui/material";
+import {styled} from "@mui/system";
+
+const FlexBetween =styled(Box)({
+    display:"flex",
+    justifyContent: "space-between",
+    assignItems: "center"
+});
+export default FlexBetween;
--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,4 +1,35 @@
-const LoginPage=()=>{
-    return (<div>loginpage</div>);
-};
+import {Box, Typography, useTheme, useMediaQuery} from "@mui/material";
+import Form from "./Form"
+const LoginPage =()=>{
+    const theme=useTheme();
+    const isNonMobileScreen= useMediaQuery("(min-width:1000px)");
+    return(
+        <Box>
+            <Box
+            width= "100%"
+            backgroundColor={theme.palette.background.alt}
+            p="1rem 6%"
+            textAllign="center"
+            >
+                <Typography fontWeight="bold" fontSize="32px" color="primary">
+                    Spciopedia
+                </Typography>
+            </Box>
+
+            <Box
+            width={isNonMobileScreens ? "50%": "93%"}
+            p="2rem"
+            m="2rem auto"
+            BorderRadius="1.5 rem"
+            backgroundColor={theme.palette.background.alt}
+            >
+                <Typography fontWeight="500" variant ="h5" sx={{mb:"1.5rem"}}>
+                    Welcome to Socipedia, the Social Media for Sociopaths!
+                </Typography>
+                <From/>
+                </Box>
+                </Box>
+    )
+}
+
 export default LoginPage;
